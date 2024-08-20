f272b195a8c3368814e0b7066a7356e6fd82da19
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-20 12:10:57 -0300

    updating tailwind config colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-radial-gradient':
+          'radial-gradient(hsl(214, 47%, 23%) 0%, hsl(237,49%, 15%) 100%)',
+        'scissors-gradient':
+          'linear-gradient(to bottom, hsl(39, 89%, 49%), hsl(40, 84%, 53%))',
+        'paper-gradient':
+          'linear-gradient(to bottom, hsl(230, 89%, 62%), hsl(230, 89%, 65%))',
+        'rock-gradient':
+          'linear-gradient(to bottom, hsl(349, 71%, 52%), hsl(349, 70%, 56%))',
+        'lizard-gradient':
+          'linear-gradient(to bottom, hsl(261, 73%, 60%), hsl(261, 72%, 63%))',
+        'cyan-gradient':
+          'linear-gradient(to bottom, hsl(189, 59%, 53%), hsl(189, 58%, 57%))',
+      },
+    },
   },
   plugins: [],
 };
