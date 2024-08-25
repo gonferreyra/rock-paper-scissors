0cbd7df4d4f8fdccc553e659a9a17f5289917ca2
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-25 17:44:11 -0300

    style changes on md and lg screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import clsx from 'clsx';
-
-import Rules from './components/Rules';
-
 import { useGameContext } from './context/GameContextProvider';
+import Rules from './components/Rules';
 
 const choices = ['rock', 'paper', 'scissors'];
 
@@ -68,37 +66,37 @@ function App() {
         </div>
       </header>
 
-      <main className="relative mx-auto flex min-h-[75vh] max-w-[500px] flex-col items-center justify-center lg:max-w-screen-sm">
+      <main className="relative mx-auto flex min-h-[75vh] max-w-[500px] flex-col items-center justify-between lg:max-w-screen-md">
         {selectedPick.length <= 0 ? (
-          <div className="relative mx-auto my-28 h-80 w-[270px] bg-bg-triangle bg-contain bg-center bg-no-repeat sm:w-[320px]">
+          <div className="relative mx-auto mb-8 mt-28 h-80 w-[270px] bg-bg-triangle bg-contain bg-center bg-no-repeat sm:w-[400px]">
             <button
               id="paper"
-              className="shadow-blue absolute left-0 top-0 flex h-24 w-24 items-center justify-center rounded-full bg-white"
+              className="shadow-blue absolute -top-4 left-0 flex h-24 w-24 items-center justify-center rounded-full bg-white transition hover:scale-105 focus:scale-105 md:-top-12 md:h-32 md:w-32"
               onClick={() => startGame('paper')}
             >
               <img src="/icon-paper.svg" alt="paper-icon" />
             </button>
             <button
               id="scissors"
-              className="shadow-yellow absolute right-0 top-0 flex h-24 w-24 items-center justify-center rounded-full bg-white"
+              className="shadow-yellow absolute -top-4 right-0 flex h-24 w-24 items-center justify-center rounded-full bg-white transition hover:scale-105 focus:scale-105 md:-top-12 md:h-32 md:w-32"
               onClick={() => startGame('scissors')}
             >
               <img src="/icon-scissors.svg" alt="scissors-icon" />
             </button>
             <button
               id="rock"
-              className="shadow-red absolute bottom-0 left-1/2 flex h-24 w-24 -translate-x-1/2 transform items-center justify-center rounded-full bg-white"
+              className="shadow-red absolute bottom-0 left-1/2 flex h-24 w-24 -translate-x-1/2 transform items-center justify-center rounded-full bg-white transition hover:scale-105 focus:scale-105 md:h-32 md:w-32"
               onClick={() => startGame('rock')}
             >
               <img src="/icon-rock.svg" alt="rock-icon" />
             </button>
           </div>
         ) : (
-          <div className="relative mx-auto my-28 flex w-[270px] flex-row justify-between lg:w-[580px] lg:gap-12">
+          <div className="relative mx-auto mb-8 mt-28 flex w-[270px] flex-row justify-between md:w-[400px] lg:w-[580px] lg:gap-12">
             <div className="flex flex-col items-center gap-6 lg:flex-col-reverse">
               <div
                 className={clsx(
-                  'flex h-24 w-24 flex-col items-center justify-center rounded-full bg-white lg:h-36 lg:w-36',
+                  'flex h-24 w-24 flex-col items-center justify-center rounded-full bg-white md:h-32 md:w-32',
                   {
                     'shadow-blue': selectedPick === 'paper',
                     'shadow-yellow': selectedPick === 'scissors',
@@ -116,7 +114,7 @@ function App() {
                 <p className="text-4xl font-bold lg:text-center">{results}</p>
                 {/* make reusable component */}
                 <button
-                  className="mt-4 flex h-12 w-full items-center justify-center rounded-md bg-white text-sm uppercase text-text-dark"
+                  className="mt-4 flex h-12 w-full items-center justify-center rounded-md bg-white text-sm uppercase text-text-dark transition hover:text-red-500"
                   onClick={() => {
                     handleSelectedPick('');
                     handleComputerPick('');
@@ -133,7 +131,7 @@ function App() {
               <div className="flex flex-col items-center gap-6 lg:flex-col-reverse">
                 <div
                   className={clsx(
-                    'flex h-24 w-24 flex-col items-center justify-center rounded-full bg-white lg:h-36 lg:w-36',
+                    'flex h-24 w-24 flex-col items-center justify-center rounded-full bg-white md:h-32 md:w-32',
                     {
                       'shadow-blue': computerPick === 'paper',
                       'shadow-yellow': computerPick === 'scissors',
@@ -147,7 +145,7 @@ function App() {
               </div>
             ) : (
               <div className="flex flex-col items-center gap-6 lg:flex-col-reverse">
-                <div className="flex h-24 w-24 flex-col items-center justify-center rounded-full bg-background-radial-gradient lg:lg:h-36 lg:w-36" />
+                <div className="flex h-24 w-24 flex-col items-center justify-center rounded-full bg-background-radial-gradient md:h-32 md:w-32" />
                 <p className="text-xs lg:text-lg">house pick</p>
               </div>
             )}
@@ -159,7 +157,7 @@ function App() {
             <p className="text-4xl font-bold">{results}</p>
             {/* make resusable component with the ither playagain btn */}
             <button
-              className="mt-4 flex h-12 w-full items-center justify-center rounded-md bg-white text-sm uppercase text-text-dark"
+              className="mt-4 flex h-12 w-full items-center justify-center rounded-md bg-white text-sm uppercase text-text-dark transition hover:text-red-500"
               onClick={() => {
                 handleSelectedPick('');
                 handleComputerPick('');
@@ -174,7 +172,7 @@ function App() {
 
         <button
           onClick={handleRulesModal}
-          className="mb-4 mt-auto flex h-12 w-32 items-center justify-center rounded-md border-2 border-white tracking-widest lg:ml-auto"
+          className="mb-4 mt-auto flex h-12 w-32 items-center justify-center rounded-md border-2 border-white tracking-widest transition hover:scale-105 focus:scale-105 lg:ml-auto"
         >
           RULES
         </button>
