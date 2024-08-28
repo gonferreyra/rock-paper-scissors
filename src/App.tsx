411a67e1fa04c227a8510f22a8434bc454b5afc2
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-28 09:51:19 -0300

    redux replaced with zustand in all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Rules from './components/Rules';
 import Header from './components/Header';
 import Game from './components/Game';
-import { useSelector } from 'react-redux';
-import { RootState } from './store/store';
+import { useGameStore } from './store/gameStore';
 
 function App() {
-  const isRulesOpen = useSelector((state: RootState) => state.game.isRulesOpen);
+  const isRulesOpen = useGameStore((state) => state.isRulesOpen);
 
   return (
     <div className="box-border min-h-screen w-full bg-background-radial-gradient p-6 font-barlow font-semibold uppercase text-white">
